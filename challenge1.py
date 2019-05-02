d13d930492800542e218a8d48e07638e53f0a060
Benjamin Girard <benjamin.girard.protic@gmail.com>
2019-05-01 23:15:38 -0400

    1 et 3 sont un peu mieux

--- challenge1.py
+++ challenge1.py
@@ -1,10 +1,7 @@
 def c(f,s):
-    o="";j=0;x=0
-    for i in range(len(f)):
+    o='';j=0;x=0
+    for i in f:
         if s[j]==' ':j+=1
-        d=int(f[i].lower()==s[j].lower())
-        o+='['*d*(x^1)+']'*x*(d^1)+f[i];j+=d;x=d
-    return o +']'*d
-
-
-print(c("She believed", "He lied"))
+        d=i.lower()==s[j].lower()
+        o+='['*d*(x^1)+']'*x*(d^1)+i;j+=d;x=d
+    return o+']'*d
--- challenge4.py
+++ challenge4.py
@@ -1,41 +1,10 @@
-"""
-Input: (3 * 4) * 2 + 1
->>> 3 4 * 2 * 1 +
-
-Input: (5 - 6) * 2 / (1 + 3)
->>> 5 6 - 2 * 1 3 + /
-
-Input: (4 - 1) / (2 - 3)
->>> 4 1 - 2 3 - /
-
-Input: ((5 - 3) * ((4 / 2 - 1) * 2))
->>> 5 3 - 4 2 / 1 - 2 * *
-
-Input: (4 ^ 2) * 3
->>> 4 2 ^ 3 *
-
-Input: 4 ^ (3 - 1)
->>> 4 3 1 - ^
-"""
-
-# Ca ressemble à quelque chose qui peut être fait recursivement. Peut-être avec moins de lignes de code?
 def solve(e):
-    e=e.replace(' ', '')
-    p=[([],[])] # il y a probablement une manière plus clean de faire ca
-    n=[];o=[]
+    p=[[[],[]]];n=[];o=[]
     for c in e:
-        if c=='(':
-            p+=[(n,o)];n=[];o=[]
-        elif c.isdigit():
-            n+=[c]
-        elif c==')':
-            p[-1][0].append(n[0]);n,o=p.pop()
-        else:
-            o+=[c]
-        if len(n)==2and o:
-            n+=[n.pop(0).strip()+' '+n.pop(0).strip()+' '+o.pop(0)+' ']
-    return n[0][:-1]
-
-#print(solve('(5 - 6) * 2 / (1 + 3)'))
-#print(c('(5 - 6) * 2 / (1 + 3)'))
-#print(c('((5 - 3) * ((4 / 2 - 1) * 2))'))
+        if c=='(':p+=[[n,o]];n=[];o=[]
+        elif c.isdigit():n+=[c]
+        elif c==')':p[-1][0]+=[n[0]];n,o=p.pop()
+        elif c==' ':pass
+        else:o+=[c]
+        if len(n)==2and o:n+=[n.pop(0)+' '+n.pop(0)+' '+o.pop(0)]
+    return n[0]
