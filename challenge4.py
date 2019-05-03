c52abf1be717789f11bdc8366a80a2c94a735159
Benjamin Girard <benjamin.girard.protic@gmail.com>
2019-05-02 20:14:39 -0400

    Challenge4 marche avec des chiffres qui sont plus grands que 1 digit maintenant

--- challenge4.py
+++ challenge4.py
@@ -1,4 +1,6 @@
-def solve(e):
+"""
+#ERREURS S'ILS Y A DES CHIFFRES >= 10 parce que le chiffre n'est pas juste un digit.
+def solve2(e):
     p=[[[],[]]];n=[];o=[]
     for c in e:
         if c=='(':p+=[[n,o]];n=[];o=[]
@@ -7,4 +9,22 @@ def solve(e):
         elif c==' ':pass
         else:o+=[c]
         if len(n)==2and o:n+=[n.pop(0)+' '+n.pop(0)+' '+o.pop(0)]
+    return n[0]
+"""
+
+#Fonctionne mais c'est rendu un mastodonte
+def solve(e):
+    p=[[[],[]]];n=[];o=[];x=0;l=''
+    for c in e:
+        d=c.isdigit()
+        if not d and x:n+=[l];l=''
+        if len(n)==2and o:n+=[n.pop(0)+' '+n.pop(0)+' '+o.pop(0)]
+        if c=='(':p+=[[n,o]];n=[];o=[]
+        elif d:l+=c
+        elif c==')':p[-1][0]+=[n[0]];n,o=p.pop()
+        elif c==' ':pass
+        else:o+=[c]
+        x=d
+    if len(n)==2:n+=[n.pop(0)+' '+n.pop(0)+' '+o.pop(0)]
+    elif x:n+=[l];n+=[n.pop(0)+' '+n.pop(0)+' '+o.pop(0)]
     return n[0]
--- challenge4_test.py
+++ challenge4_test.py
@@ -5,18 +5,27 @@ class TestChallenge4(object):
     def test_challenge_4_1(self):
         equation='(3 * 4) * 2 + 1'
         assert challenge4.solve(equation) == '3 4 * 2 * 1 +'
+
     def test_challenge_4_2(self):
         equation='(5 - 6) * 2 / (1 + 3)'
         assert challenge4.solve(equation) == '5 6 - 2 * 1 3 + /'
+
     def test_challenge_4_3(self):
         equation='(4 - 1) / (2 - 3)'
         assert challenge4.solve(equation) == '4 1 - 2 3 - /'
+
     def test_challenge_4_4(self):
         equation='((5 - 3) * ((4 / 2 - 1) * 2))'
         assert challenge4.solve(equation) == '5 3 - 4 2 / 1 - 2 * *'
+
     def test_challenge_4_5(self):
         equation='(4 ^ 2) * 3'
         assert challenge4.solve(equation) == '4 2 ^ 3 *'
+
     def test_challenge_4_6(self):
         equation='4 ^ (3 - 1)'
         assert challenge4.solve(equation) == '4 3 1 - ^'
+
+    def test_challenge_4_7(self):
+        equation='((4+7)*12+2)/4'
+        assert challenge4.solve(equation) == '4 7 + 12 * 2 + 4 /'
